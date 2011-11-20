76e10773e36fda4ea1ca4156511045b983d0fb28
Nicolas R Dufour <nrdufour@gmail.com>
2011-11-19 21:10:17 -0500

    Add a state in the processor.

--- src/ecsv.erl
+++ src/ecsv.erl
@@ -6,30 +6,38 @@
 -author("Nicolas R Dufour <nicolas.dufour@nemoworld.info>").
 
 -export([process_csv_file_with/2, process_csv_string_with/2]).
+-export([process_csv_file_with/3, process_csv_string_with/3]).
 
 process_csv_file_with(IoDevice, RowFunction) ->
-    ProcessingPid = self(),
-    ParsingPid = spawn(ecsv_parser, start_parsing, [ProcessingPid]),
-    _ReadingPid = spawn(ecsv_reader, stream_from_file, [IoDevice, ParsingPid]),
-
-    loop(RowFunction),
-    ok.
+    process_csv_file_with(IoDevice, RowFunction, []).
 
 process_csv_string_with(String, RowFunction) ->
+    process_csv_string_with(String, RowFunction, []).
+
+process_csv_file_with(IoDevice, RowFunction, InitState) ->
+    do_it(IoDevice, RowFunction, stream_from_file, InitState).
+
+process_csv_string_with(String, RowFunction, InitState) ->
+    do_it(String, RowFunction, stream_from_string, InitState).
+
+%
+% Internal API
+%
+
+do_it(Stream, RowFunction, StreamFunctionName, InitState) ->
     ProcessingPid = self(),
     ParsingPid = spawn(ecsv_parser, start_parsing, [ProcessingPid]),
-    _ReadingPid = spawn(ecsv_reader, stream_from_string, [String, ParsingPid]),
+    _ReadingPid = spawn(ecsv_reader, StreamFunctionName, [Stream, ParsingPid]),
 
-    loop(RowFunction),
-    ok.
+    loop(RowFunction, InitState).
 
-loop(RowFunction) ->
+loop(RowFunction, State) ->
     receive
         {newline, [[]]} ->
-            loop(RowFunction);
+            loop(RowFunction, State);
         {newline, NewLine} ->
-            RowFunction(NewLine),
-            loop(RowFunction);
+            NewState = RowFunction(NewLine, State),
+            loop(RowFunction, NewState);
         {done} ->
-            ok
+            {ok, State}
     end.
